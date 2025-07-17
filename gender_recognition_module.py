3a95cb0541ff288e96892a75f5f8ee23f3580df8
Himanshu-738 <himanshuyadav1961@gmail.com>
2025-07-17 17:00:20 +0530

    Add files via upload

---
+++ gender_recognition_module.py
@@ -0,0 +1,36 @@
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
+import tensorflow as tf 
+import tensorflow_datasets as tfds
+import os
+from PIL import Image
+import pickle 
+
+class genderRecognition:
+    def __init__(self, model_file):
+        with open(model_file, 'rb') as file:
+            self.model = pickle.load(file)
+
+        self.img_height = 128
+        self.img_width = 128
+
+    def load_process_image(self, image_path):
+        img = Image.open(image_path)
+        img = tf.image.resize(img, [128, 128])
+        img = img / 255.0
+        img_array = tf.expand_dims(img, axis=0)  # Add batch dimension
+        return img_array
+
+    def predict_gender(self, img_path):
+        img_array = self.load_process_image(img_path)
+        predictions = self.model.predict(img_array)[0]
+        predicted_class = np.argmax(predictions)
+        probability = np.max(predictions)
+        gender = 'Male' if predicted_class == 0 else 'Female'
+        return gender, probability
+
+
